f5b530c914fe08c6154bd085547f7eb41f30676a
yuuta-git12 <tayu2201@icloud.com>
2024-11-26 11:51:38 +0900

    イベント・スタイルの適用方法の追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 export const App = () => {
+    const onClickButtton = () => alert();
+    // スタイルのオブジェクト
+    const contentStyle = {
+        color: "blue",
+        fontSize: "25px",
+        margin: "100px",
+    };
     return(
     // ReactFragmentを使うパターン 
     <>
-        <h1>こんにちは!</h1>
-        <p>おげんきですか?</p>
+        {/* スタイルの適用方法 */}
+        <h1 style={{ color: "#aacfd0" }}>こんにちは!</h1>
+        <p style={contentStyle}>おげんきですか?</p>
+        {/* 関数の適用方法 */}
+        <button onClick={onClickButtton}>ボタン</button>
     </>
     ); 
 };
